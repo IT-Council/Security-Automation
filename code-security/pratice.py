530f67d107d16a68032e6adbccca9fb2ece4dce1
Your Name <you@example.com>
2026-05-30 01:03:16 +0530

    updated salary credit logic

---
+++ code-security/pratice.py
@@ -0,0 +1,29 @@
+from datetime import date
+
+
+print ("Hello Cybersecurity")
+
+if 5 > 10:
+    print ("5 is true")
+else:   print (
+"5 is not true"
+)
+
+name = "cybersecurity"
+for i in range(1):
+    print (name)
+
+## Example 2 
+
+from datetime import date 
+
+Name = "Mahesh babu"
+age = 40 
+is_employed = True
+salary = 150000.00
+bonus = 10.0
+salary_credit_data = date(2024, 6, 30)
+
+if date.today() > salary_credit_data:
+    print ("Salary credited")
+else:    print ("Salary not credited yet")
--- code-security/security.py
+++
@@ -1,9 +0,0 @@
-print ("Hello Cybersecurity")
-
-if 5 > 10:
-    print ("5 is true")
-else:   print (
-"5 is not true"
-)
-
-   
